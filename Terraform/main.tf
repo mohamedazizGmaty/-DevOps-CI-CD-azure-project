965a7982da639fd685c7e77251eaecba024b95b3
mohamedazizGmaty <azizgmt817@gmail.com>
2025-10-04 00:02:23 +0100

    1.1.3

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,11 +11,13 @@ provider "azurerm" {
   features {}
 }
 
+# Resource Group
 resource "azurerm_resource_group" "devops_rg" {
   name     = "mydevops-rg"
-  location = "East US"
+  location = "uksouth"
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "devops-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -23,6 +25,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.devops_rg.name
 }
 
+# Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "devops-subnet"
   resource_group_name  = azurerm_resource_group.devops_rg.name
@@ -30,6 +33,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# Network Security Group
 resource "azurerm_network_security_group" "nsg" {
   name                = "devops-nsg"
   location            = azurerm_resource_group.devops_rg.location
@@ -43,9 +47,10 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*"  # Limitez à votre IP après
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
   security_rule {
     name                       = "HTTP"
     priority                   = 1002
@@ -57,6 +62,7 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
   security_rule {
     name                       = "Jenkins"
     priority                   = 1003
@@ -68,6 +74,7 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
   security_rule {
     name                       = "K8s"
     priority                   = 1004
@@ -81,20 +88,26 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+# Public IPs
 resource "azurerm_public_ip" "jenkins_pip" {
   name                = "jenkins-pip"
   location            = azurerm_resource_group.devops_rg.location
   resource_group_name = azurerm_resource_group.devops_rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"  # Standard SKU nécessite Static
+  sku                 = "Standard"
+  zones               = ["1"]     # Optionnel, pour disponibilité  
 }
 
 resource "azurerm_public_ip" "k3s_pip" {
   name                = "k3s-pip"
   location            = azurerm_resource_group.devops_rg.location
   resource_group_name = azurerm_resource_group.devops_rg.name
-  allocation_method   = "Dynamic"
-}
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = ["1"]
+  }
 
+# Jenkins Network Interface
 resource "azurerm_network_interface" "jenkins_nic" {
   name                = "jenkins-nic"
   location            = azurerm_resource_group.devops_rg.location
@@ -113,28 +126,34 @@ resource "azurerm_network_interface_security_group_association" "jenkins_nsg" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
+# Jenkins VM
 resource "azurerm_linux_virtual_machine" "jenkins" {
   name                = "jenkins-vm"
   resource_group_name = azurerm_resource_group.devops_rg.name
   location            = azurerm_resource_group.devops_rg.location
   size                = "Standard_B1s"
   admin_username      = "azureuser"
+
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
+    public_key = file("C:\\Users\\azeez\\.ssh\\id_rsa.pub")  
+    }
+
   disable_password_authentication = true
   network_interface_ids           = [azurerm_network_interface.jenkins_nic.id]
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-focal"
     sku       = "20_04-lts-gen2"
     version   = "latest"
   }
+
   custom_data = base64encode(<<-EOF
     #!/bin/bash
     apt update -y
@@ -145,12 +164,68 @@ resource "azurerm_linux_virtual_machine" "jenkins" {
   )
 }
 
-# Dupliquez pour K3s VM (changez noms : k3s-nic, k3s-vm, k3s-pip)
-# Exemple simplifié, ajoutez le bloc similaire
+# K3s Network Interface
+resource "azurerm_network_interface" "k3s_nic" {
+  name                = "k3s-nic"
+  location            = azurerm_resource_group.devops_rg.location
+  resource_group_name = azurerm_resource_group.devops_rg.name
 
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.k3s_pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "k3s_nsg" {
+  network_interface_id      = azurerm_network_interface.k3s_nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# K3s VM
+resource "azurerm_linux_virtual_machine" "k3s" {
+  name                = "k3s-vm"
+  resource_group_name = azurerm_resource_group.devops_rg.name
+  location            = azurerm_resource_group.devops_rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  disable_password_authentication = true
+  network_interface_ids           = [azurerm_network_interface.k3s_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<-EOF
+    #!/bin/bash
+    apt update -y
+    apt install -y docker.io
+    systemctl start docker
+    usermod -aG docker azureuser
+  EOF
+  )
+}
+
+# Outputs
 output "jenkins_ip" {
   value = azurerm_public_ip.jenkins_pip.ip_address
 }
+
 output "k3s_ip" {
   value = azurerm_public_ip.k3s_pip.ip_address
 }
